aa9717a98894c692c52848057dcbfa34d06fd981
Lara Pereira <larabernspereira@gmail.com>
2024-10-10 19:40:12 -0300

    🚧 PROGRESS: add queries for exercise 1 and 2

--- python/consultas_python.py
+++ python/consultas_python.py
@@ -6,11 +6,18 @@ data_frame_funcionarios = pd.read_csv(r'C:\Users\Lara\Documents\TP-3-Bloco\csv\f
 data_frame_dependentes = pd.read_csv(r'C:\Users\Lara\Documents\TP-3-Bloco\csv\dependentes.csv', encoding='latin-1')
 data_frame_historico_salarios = pd.read_csv(r'C:\Users\Lara\Documents\TP-3-Bloco\csv\historico_salarios.csv', encoding='latin-1')
 
-# Função exemplo de consulta usando query
-def consultar_salarios():
-    cargos_filtrados = data_frame_cargos.query('8000 <= salario_base <= 50000')
-    print("Cargos com salário base entre 8.000 e 50.000:")
-    print(cargos_filtrados.to_string())
+def listar_crescente(data_frame, coluna):
+    data_frame_ordenado = data_frame.sort_values(by=coluna)
+    print(data_frame_ordenado.to_string())
 
-# Executar a consulta
-consultar_salarios()
+# 1 - Listar individualmente as tabelas de: Funcionários, Cargos, Departamentos, Histórico de Salários e Dependentes em ordem crescente.
+print("Tabela Funcionários em ordem crescente - Nome:")
+listar_crescente(data_frame_funcionarios, "nome")
+print("Tabela Cargos em ordem crescente - Descricao:")
+listar_crescente(data_frame_cargos, "descricao")
+print("Tabela Departamentos em ordem crescente - Nome:")
+listar_crescente(data_frame_departamentos, "nome")
+print("Tabela Histórico de Salários em ordem crescente - salario_recebido:")
+listar_crescente(data_frame_historico_salarios, "salario_recebido")
+print("Tabela Dependentes em ordem crescente - nome:")
+listar_crescente(data_frame_dependentes, "nome")
--- python/consultas_sql.py
+++ python/consultas_sql.py
@@ -3,28 +3,30 @@ from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
 
-# Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
 
-# Obter os valores das variáveis de ambiente
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
 
-# Configuração da conexão com o banco de dados PostgreSQL
-engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+engine = create_engine(
+    f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
 # Função de consulta dinâmica
 def consultar(query):
     with engine.connect() as connection:
         result = connection.execute(text(query))
-        
-        # Converter o resultado em um DataFrame para exibição
         df = pd.DataFrame(result.fetchall(), columns=result.keys())
         print(df)
 
-# Exemplo de uso da função com uma query dinâmica
-query_exemplo = "SELECT * FROM funcionarios"
-consultar(query_exemplo)
+# 2 - Listar os funcionários, com seus cargos, departamentos e os respectivos dependentes
+print("Listar os funcionários, com seus cargos, departamentos e os respectivos dependentes:")
+consultar("""
+          SELECT funcionarios.nome as nome_funcionario, cargos.descricao as nome_cargo, departamentos.nome as nome_departamento, dependentes.nome as nome_dependente
+          FROM funcionarios
+          INNER JOIN cargos ON funcionarios.id_cargo = cargos.id_cargo
+          INNER JOIN departamentos ON cargos.id_departamento = departamentos.id_departamento 
+          INNER JOIN dependentes ON funcionarios.id_funcionario = dependentes.id_funcionario 
+          """)
